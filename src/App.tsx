bff3077895e608e578569bdd2c98d67d75d58cf1
Rahmatulloh <komilov2007.2007@gmail.com>
2026-05-12 18:41:07 +0500

    added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import DashboardRouter from "./router/DashboardRouter";
 function App() {
   const isAuth = document.cookie.includes("access_token=");
 
-  return isAuth ? <DashboardRouter /> : <AuthRouter />;
+  // return isAuth ? <DashboardRouter /> : <AuthRouter />;
+  return <DashboardRouter />;
 }
 
 export default App;
